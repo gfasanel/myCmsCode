020bf026af46c79699f927396a41cce562448e7e
treis <>
2011-10-17 09:16:55 +0000

    - added ability to plot on log scale and integrated plot - added more MC samples - plotstyle

--- HEEPemu/macro_MakeEMuInvMassLSOSPlot.C
+++ HEEPemu/macro_MakeEMuInvMassLSOSPlot.C
@@ -1,150 +1,257 @@
 {
+  #include <string>
+  #include <sstream>
 
+  #include "TH1F.h"
   //#include "plotstyle.C"
-  
-  //TFile input("EMu_1092pb_QCD10pc_Trig94pc_PVXinfo_EE40_electrons_PURew28jun__11July.root","open");
-  TFile input("testEmuSpec.root","open");
+ 
+  // parameters //////////////////////////////////////////////////////////////
+  // luminosity of data 
+  TFile input("testEmuSpec.root", "open");
+  const float lumi = 3190;
+
+  bool logPlot = false;
+  bool integrate = false;
+  ////////////////////////////////////////////////////////////////////////////
 
   input.cd();
 
   // INTEGRATING FROM THE RIGHT SIDE!
+  if (integrate) { 
+    // //LOOPING ON BINS
+    double error;
+    for (int i = 1; i < 101; ++i) {
+      LS_ttbar->SetBinContent(i, LS_ttbar->IntegralAndError(i, 100, error));
+      LS_ttbar->SetBinError(i, error);
+      LS_ztautau->SetBinContent(i, LS_ztautau->IntegralAndError(i, 100, error));
+      LS_ztautau->SetBinError(i, error);
+      LS_ww->SetBinContent(i, LS_ww->IntegralAndError(i, 100, error));
+      LS_ww->SetBinError(i, error);
+      LS_wz->SetBinContent(i, LS_wz->IntegralAndError(i, 100, error));
+      LS_wz->SetBinError(i, error);
+      LS_tw->SetBinContent(i, LS_tw->IntegralAndError(i, 100, error));
+      LS_tw->SetBinError(i, error);
+      LS_wjet->SetBinContent(i, LS_wjet->IntegralAndError(i, 100, error));
+      LS_wjet->SetBinError(i, error);
+      LS_zmum->SetBinContent(i, LS_zmum->IntegralAndError(i, 100, error));
+      LS_zmum->SetBinError(i, error);
+      LS_zele->SetBinContent(i, LS_zele->IntegralAndError(i, 100, error));
+      LS_zele->SetBinError(i, error);
+      LS_data->SetBinContent(i, LS_data->IntegralAndError(i, 100, error));   
+      LS_data->SetBinError(i, error);
 
-  // //LOOPING ON BINS
-//   for (int i = 1; i<101; i++) {
-//     LS_ttbar->SetBinContent(i, LS_ttbar->Integral(i,100) );
-//     LS_ztautau->SetBinContent(i, LS_ztautau->Integral(i,100) );
-//     LS_ww->SetBinContent(i, LS_ww->Integral(i,100) );
-//     LS_wjet->SetBinContent(i, LS_wjet->Integral(i,100) );
-//     LS_data->SetBinContent(i, LS_data->Integral(i,100) );   
-//   }     
+      OS_ttbar->SetBinContent(i, OS_ttbar->IntegralAndError(i, 100, error));
+      OS_ttbar->SetBinError(i, error);
+      OS_ztautau->SetBinContent(i, OS_ztautau->IntegralAndError(i, 100, error));
+      OS_ztautau->SetBinError(i, error);
+      OS_ww->SetBinContent(i, OS_ww->IntegralAndError(i, 100, error));
+      OS_ww->SetBinError(i, error);
+      OS_wz->SetBinContent(i, OS_wz->IntegralAndError(i, 100, error));
+      OS_wz->SetBinError(i, error);
+      OS_tw->SetBinContent(i, OS_tw->IntegralAndError(i, 100, error));
+      OS_tw->SetBinError(i, error);
+      OS_wjet->SetBinContent(i, OS_wjet->IntegralAndError(i, 100, error));
+      OS_wjet->SetBinError(i, error);
+      OS_zmum->SetBinContent(i, OS_zmum->IntegralAndError(i, 100, error));
+      OS_zmum->SetBinError(i, error);
+      OS_zele->SetBinContent(i, OS_zele->IntegralAndError(i, 100, error));
+      OS_zele->SetBinError(i, error);
+      OS_data->SetBinContent(i, OS_data->IntegralAndError(i, 100, error));   
+      OS_data->SetBinError(i, error);
+    }     
+  }
   
   //setPlotStyle();
-  
+ 
+  TCanvas *cLS = new TCanvas("cLS", "emu Spectrum LS", 100, 100, 800, 600);
+  cLS->SetBorderMode(0);
+  cLS->SetFrameBorderMode(0);
+  cLS->SetFillColor(0);
+  cLS->SetFrameFillColor(0);
+  if (logPlot) cLS->SetLogy();
+  TCanvas *cOS = new TCanvas("cOS", "emu Spectrum OS", 100, 100, 800, 600);
+  cOS->SetBorderMode(0);
+  cOS->SetFrameBorderMode(0);
+  cOS->SetFillColor(0);
+  cOS->SetFrameFillColor(0);
+  if (logPlot) cOS->SetLogy();
+ 
   gStyle->SetOptStat(0);
   gStyle->SetOptTitle(0);
   
-  gStyle->SetTitleXOffset(0.9);
+  gStyle->SetTitleXOffset(1.);
   gStyle->SetTitleYOffset(1.3);
 
+  ////////////////////////////////////////////////////////////////////////////
   // LS---------
+  cLS->cd();
   
-   LS_ttbar->GetXaxis()->SetTitle("M_{e #mu} (GeV/c^{2})");
-   LS_ttbar->GetXaxis()->SetTitleSize(0.047);
-   LS_ttbar->GetXaxis()->SetTitleOffset(0.9);
-   LS_ttbar->GetXaxis()->SetLabelSize(0.040);
+  LS_ttbar->GetXaxis()->SetTitle("M_{e#mu} (GeV/c^{2})");
+  LS_ttbar->GetXaxis()->SetTitleSize(0.04);
+  LS_ttbar->GetXaxis()->SetLabelSize(0.04);
 
-   LS_ttbar->GetYaxis()->SetTitle("Nb LS events  / 10 GeV/c^{2}");
-   LS_ttbar->GetYaxis()->SetTitleSize(0.047);
-   LS_ttbar->GetYaxis()->SetTitleOffset(1.3);
+  if (integrate) LS_ttbar->GetYaxis()->SetTitle("# of LS events >= M_{e#mu}");
+  else LS_ttbar->GetYaxis()->SetTitle("# of LS events / 10 GeV/c^{2}");
+  LS_ttbar->GetYaxis()->SetTitleSize(0.04);
+  LS_ttbar->GetYaxis()->SetTitleOffset(1.2);
 
-   //
-   LS_ttbar->SetFillColor(2);
-   LS_ttbar->Draw("HIST");
+  //
+  LS_ttbar->SetFillColor(2);
+  LS_ttbar->Draw("HIST");
 
-   LS_ztautau->SetFillColor(6);
-   LS_ztautau->Draw("HISTsames");
+  LS_ztautau->SetFillColor(6);
+  LS_ztautau->Draw("HISTsames");
 
-   LS_ww->SetFillColor(5);
-   LS_ww->Draw("HISTsames");
+  LS_ww->SetFillColor(5);
+  LS_ww->Draw("HISTsames");
 
-   LS_wjet->SetFillColor(4);
-   LS_wjet->Draw("HISTsames");
+  LS_wz->SetFillColor(8);
+  LS_wz->Draw("HISTsames");
 
-   //LS_zmum->SetFillColor(3);
-   //LS_zmum->Draw("HISTsames");
+  LS_tw->SetFillColor(46);
+  LS_tw->Draw("HISTsames");
 
-   //LS_zele->SetFillColor(7);
-   //LS_zele->Draw("HISTsames");
+  LS_wjet->SetFillColor(4);
+  LS_wjet->Draw("HISTsames");
 
-   //
-   LS_data->SetLineWidth(2);
-   LS_data->Draw("sames");
+  LS_zmum->SetFillColor(7);
+  LS_zmum->Draw("HISTsames");
 
-   //Redraw axis
-   LS_ttbar->Draw("sameaxis");
+  LS_zele->SetFillColor(3);
+  LS_zele->Draw("HISTsames");
 
-   //
-   TLegend legend0(0.48,0.60,0.68,0.88);
-   legend0.SetTextSize(0.04);
-   legend0.SetFillColor(0);
+  //
+  LS_data->SetLineWidth(2);
+  LS_data->SetMarkerStyle(8);
+  LS_data->SetMarkerSize(0.8);
+  LS_data->Draw("sames");
 
-   legend0.AddEntry(LS_data, "Data");
-   legend0.AddEntry(LS_ttbar, "t #bar{t} (MC)");
-   legend0.AddEntry(LS_ztautau, "Z #rightarrow #tau #tau (MC)");
-   legend0.AddEntry(LS_ww, "WW, (MC)");
-   legend0.AddEntry(LS_wjet, "W+jet + Z #rightarrow #mu #mu (MC)");
+  double dataMax = LS_data->GetMaximum();
+  double ttbarMax = LS_ttbar->GetMaximum();
+  if (dataMax > ttbarMax) LS_ttbar->SetMaximum(dataMax * 1.1);
+  //if (LS_data->GetMaximum() > LS_ttbar->GetMaximum()) LS_ttbar->GetYaxis()->SetRangeUser(0., LS_data->GetMaximum() * 1.1);
 
+  //Redraw axis
+  LS_ttbar->Draw("sameaxis");
+
+  //
+  TLegend legendLS(0.7, 0.8, 0.88, 0.54);
+  legendLS.SetTextSize(0.03);
+  legendLS.SetFillColor(0);
+
+  legendLS.AddEntry(LS_data, "Data");
+  legendLS.AddEntry(LS_ttbar, "t #bar{t} (MC)");
+  legendLS.AddEntry(LS_ztautau, "Z #rightarrow #tau #tau (MC)");
+  legendLS.AddEntry(LS_ww, "WW, (MC)");
+  legendLS.AddEntry(LS_wz, "WZ, (MC)");
+  legendLS.AddEntry(LS_tw, "tW, (MC)");
+  legendLS.AddEntry(LS_wjet, "W+jet (MC)");
+  legendLS.AddEntry(LS_zmum, "Z #rightarrow #mu #mu (MC)");
+  legendLS.AddEntry(LS_zele, "Z #rightarrow ee (MC)");
+
+  legendLS.SetBorderSize(0);
+  legendLS.Draw("sames");
+  
+  TPaveLabel labelPrelimLS(0.7, 0.91, 0.9, 0.82, "CMS Preliminary", "brNDC");
+  labelPrelimLS.SetFillColor(0);
+  labelPrelimLS.SetFillStyle(0);
+  labelPrelimLS.SetBorderSize(0);
+  labelPrelimLS.SetTextSize(0.40);
+  labelPrelimLS.Draw("sames");
+
+  stringstream sStream;
+  sStream.str("");
+  sStream << "#sqrt{s} = 7TeV,  #int L dt = " << lumi << "pb^{-1}";
+  TPaveLabel labelLumiLS(0.3, 0.88, 0.65, 0.78, sStream.str().c_str(), "brNDC");
+  labelLumiLS.SetFillColor(0);
+  labelLumiLS.SetFillStyle(0);
+  labelLumiLS.SetBorderSize(0);
+  labelLumiLS.SetTextSize(0.40);
+  labelLumiLS.Draw("sames");
+
+  ////////////////////////////////////////////////////////////////////////////
   // OS-------
-//
-//  OS_ttbar->GetXaxis()->SetTitle("M_{e #mu} (GeV/c^{2})");
-//  OS_ttbar->GetXaxis()->SetTitleSize(0.047);
-//  OS_ttbar->GetXaxis()->SetTitleOffset(0.9);
-//  OS_ttbar->GetXaxis()->SetLabelSize(0.040);
-//
-//  OS_ttbar->GetYaxis()->SetTitle("Nb OS events / 10 GeV/c^{2}");
-//  OS_ttbar->GetYaxis()->SetTitleSize(0.047);
-//  OS_ttbar->GetYaxis()->SetTitleOffset(1.3);
-//  
-//  //
-//  OS_ttbar->SetFillColor(2);
-//  OS_ttbar->Draw("HIST");
-//
-//  OS_ztautau->SetFillColor(6);
-//  OS_ztautau->Draw("HISTsames");
-//
-//  OS_ww->SetFillColor(5);
-//  OS_ww->Draw("HISTsames");
-//
-//  OS_wjet->SetFillColor(4);
-//  OS_wjet->Draw("HISTsames");
-//
-////   OS_zmum->SetFillColor(3);
-////   OS_zmum->Draw("HISTsames");
-//
-////   OS_zele->SetFillColor(7);
-////   OS_zele->Draw("HISTsames");
-//
-//  //
-//  OS_data->SetLineWidth(2);
-//  OS_data->Draw("sames");
-//
-//  //Redraw axis
-//  OS_ttbar->Draw("sameaxis");
-//
-//  //
-//  TLegend legend0(0.48,0.60,0.68,0.88);
-//  legend0.SetTextSize(0.04);
-//  legend0.SetFillColor(0);
-//
-//  legend0.AddEntry(OS_data, "Data");
-//  legend0.AddEntry(OS_ttbar, "t #bar{t} (MC)");
-//  legend0.AddEntry(OS_ztautau, "Z #rightarrow #tau #tau (MC)");
-//  legend0.AddEntry(OS_ww, "WW, (MC)");
-//  legend0.AddEntry(OS_wjet, "W+jet + Z #rightarrow #mu #mu (MC)");
+  cOS->cd();
+
+  OS_ttbar->GetXaxis()->SetTitle("M_{e #mu} (GeV/c^{2})");
+  OS_ttbar->GetXaxis()->SetTitleSize(0.04);
+  OS_ttbar->GetXaxis()->SetLabelSize(0.04);
+
+  if (integrate) OS_ttbar->GetYaxis()->SetTitle("# of OS events >= M_{e#mu}");
+  else OS_ttbar->GetYaxis()->SetTitle("# OS events / 10 GeV/c^{2}");
+  OS_ttbar->GetYaxis()->SetTitleSize(0.04);
+  OS_ttbar->GetYaxis()->SetTitleOffset(1.1);
   
+  OS_ttbar->SetFillColor(2);
+  OS_ttbar->Draw("HIST");
 
+  OS_ztautau->SetFillColor(6);
+  OS_ztautau->Draw("HISTsames");
 
+  OS_ww->SetFillColor(5);
+  OS_ww->Draw("HISTsames");
 
-  //---------------------------------------------------------
-  legend0.SetBorderSize(0);
-  legend0.Draw("sames");
-  
-  TPaveLabel label1(0.300,0.89,0.740,0.99,"CMS Preliminary","brNDC");
-  label1.SetFillColor(0);
-  label1.SetFillStyle(0);
-  label1.SetBorderSize(0);
-  label1.SetTextSize(0.40);
-  label1.Draw("sames");
+  OS_wz->SetFillColor(8);
+  OS_wz->Draw("HISTsames");
 
-  TPaveLabel label0(0.300,0.89,0.740,0.99,"7 TeV, #int L dt = 3190pb^{-1}","brNDC");
-  label0.SetFillColor(0);
-  label0.SetFillStyle(0);
-  label0.SetBorderSize(0);
-  label0.SetTextSize(0.40);
-  label0.Draw("sames");
+  OS_tw->SetFillColor(46);
+  OS_tw->Draw("HISTsames");
 
+  OS_wjet->SetFillColor(4);
+  OS_wjet->Draw("HISTsames");
 
-  
+  OS_zmum->SetFillColor(7);
+  OS_zmum->Draw("HISTsames");
+
+  OS_zele->SetFillColor(3);
+  OS_zele->Draw("HISTsames");
+
+  //
+  OS_data->SetLineWidth(2);
+  OS_data->SetMarkerStyle(8);
+  OS_data->SetMarkerSize(0.8);
+  OS_data->Draw("sames");
+
+  dataMax = OS_data->GetMaximum();
+  ttbarMax = OS_ttbar->GetMaximum();
+  if (dataMax > ttbarMax) OS_ttbar->SetMaximum(dataMax * 1.1);
+  //if (OS_data->GetMaximum() > OS_ttbar->GetMaximum()) OS_ttbar->SetMaximum(OS_data->GetMaximum() * 0.1);
+
+  //Redraw axis
+  OS_ttbar->Draw("sameaxis");
+
+  //
+  TLegend legendOS(0.7, 0.8, 0.88, 0.54);
+  legendOS.SetTextSize(0.03);
+  legendOS.SetFillColor(0);
+
+  legendOS.AddEntry(OS_data, "Data");
+  legendOS.AddEntry(OS_ttbar, "t #bar{t} (MC)");
+  legendOS.AddEntry(OS_ztautau, "Z #rightarrow #tau #tau (MC)");
+  legendOS.AddEntry(OS_ww, "WW, (MC)");
+  legendOS.AddEntry(OS_wz, "WZ, (MC)");
+  legendOS.AddEntry(OS_tw, "tW, (MC)");
+  legendOS.AddEntry(OS_wjet, "W+jet (MC)");
+  legendOS.AddEntry(OS_zmum, "Z #rightarrow #mu #mu (MC)");
+  legendOS.AddEntry(OS_zele, "Z #rightarrow ee (MC)");
+
+  legendOS.SetBorderSize(0);
+  legendOS.Draw("sames");
+
+  TPaveLabel labelPrelimOS(0.7, 0.91, 0.9, 0.82, "CMS Preliminary", "brNDC");
+  labelPrelimOS.SetFillColor(0);
+  labelPrelimOS.SetFillStyle(0);
+  labelPrelimOS.SetBorderSize(0);
+  labelPrelimOS.SetTextSize(0.40);
+  labelPrelimOS.Draw("sames");
+
+  TPaveLabel labelLumiOS(0.3, 0.88, 0.65, 0.78, sStream.str().c_str(), "brNDC");
+  labelLumiOS.SetFillColor(0);
+  labelLumiOS.SetFillStyle(0);
+  labelLumiOS.SetBorderSize(0);
+  labelLumiOS.SetTextSize(0.40);
+  labelLumiOS.Draw("sames");
+
   //gPad->Print("Plot_EMuMethod_R.eps");
   
 }
--- HEEPemu/macro_MakeEMuInvMassPlot.C
+++ HEEPemu/macro_MakeEMuInvMassPlot.C
@@ -1,40 +1,75 @@
 {
-
+  #include <string>
+  #include <sstream>
+      
+  #include "TH1F.h"
   //#include "plotstyle.C"
+      
+  // parameters //////////////////////////////////////////////////////////////
+  // luminosity of data 
+  TFile input("testEmuSpec.root", "open");
+  const float lumi = 3190;
+      
+  bool logPlot = false;
+  bool integrate = false;
+  ////////////////////////////////////////////////////////////////////////////
 
-  //TFile input("EMu_1092pb_QCD10pc_Trig94pc_PVXinfo_EE40_electrons_PURew28jun__11July.root","open");
   TFile input("testEmuSpec.root","open");
+  //TFile input("testEmuSpec_PUcut20_3190pb-1.root","open");
   
   input.cd();
 
   // INTEGRATING FROM THE RIGHT SIDE!
-
- //  //LOOPING ON BINS
-//   for (int i = 1; i<101; i++) {
-//     emu_ttbar->SetBinContent(i, emu_ttbar->Integral(i,100) );
-//     emu_ztautau->SetBinContent(i, emu_ztautau->Integral(i,100) );
-//     emu_ww->SetBinContent(i, emu_ww->Integral(i,100) );
-//     emu_wjet->SetBinContent(i, emu_wjet->Integral(i,100) );
-//     emu_data->SetBinContent(i, emu_data->Integral(i,100) );   
-//     //emu_data->SetBinError(i, sqrt(emu_data->GetBinContent(i) ) );   
-//   }     
-  
+   if (integrate) { 
+    // //LOOPING ON BINS
+    double error;
+    for (int i = 1; i < 101; ++i) {
+      //emu_ttbar->GetNbinsX();
+      emu_ttbar->SetBinContent(i, emu_ttbar->IntegralAndError(i, 100, error));
+      emu_ttbar->SetBinError(i, error);
+      emu_ztautau->SetBinContent(i, emu_ztautau->IntegralAndError(i, 100, error));
+      emu_ztautau->SetBinError(i, error);
+      emu_ww->SetBinContent(i, emu_ww->IntegralAndError(i, 100, error));
+      emu_ww->SetBinError(i, error);
+      emu_wz->SetBinContent(i, emu_wz->IntegralAndError(i, 100, error));
+      emu_wz->SetBinError(i, error);
+      emu_tw->SetBinContent(i, emu_tw->IntegralAndError(i, 100, error));
+      emu_tw->SetBinError(i, error);
+      emu_wjet->SetBinContent(i, emu_wjet->IntegralAndError(i, 100, error));
+      emu_wjet->SetBinError(i, error);
+      emu_zmumu->SetBinContent(i, emu_zmumu->IntegralAndError(i, 100, error));
+      emu_zmumu->SetBinError(i, error);
+      emu_zee->SetBinContent(i, emu_zee->IntegralAndError(i, 100, error));
+      emu_zee->SetBinError(i, error);
+      emu_data->SetBinContent(i, emu_data->IntegralAndError(i, 100, error));   
+      emu_data->SetBinError(i, error);
+    }     
+  }
+ 
   //setPlotStyle();
-  
+
+  TCanvas *cEmu = new TCanvas("cEmu", "emu Spectrum", 100, 100, 800, 600);
+  cEmu->SetBorderMode(0);
+  cEmu->SetFrameBorderMode(0);
+  cEmu->SetFillColor(0);
+  cEmu->SetFrameFillColor(0);
+  if (logPlot) cEmu->SetLogy();
+  cEmu->cd();
+ 
   gStyle->SetOptStat(0);
   gStyle->SetOptTitle(0);
   
-  gStyle->SetTitleXOffset(0.9);
+  gStyle->SetTitleXOffset(1.);
   gStyle->SetTitleYOffset(1.3);
   
-  emu_ttbar->GetXaxis()->SetTitle("M_{e #mu} (GeV/c^{2})");
-  emu_ttbar->GetXaxis()->SetTitleSize(0.047);
-  emu_ttbar->GetXaxis()->SetTitleOffset(0.9);
-  emu_ttbar->GetXaxis()->SetLabelSize(0.040);
+  emu_ttbar->GetXaxis()->SetTitle("M_{e#mu} (GeV/c^{2})");
+  emu_ttbar->GetXaxis()->SetTitleSize(0.04);
+  emu_ttbar->GetXaxis()->SetLabelSize(0.04);
 
-  emu_ttbar->GetYaxis()->SetTitle("Nb events / 10 GeV/c^{2}");
-  emu_ttbar->GetYaxis()->SetTitleSize(0.047);
-  emu_ttbar->GetYaxis()->SetTitleOffset(1.3);
+  if (integrate) emu_ttbar->GetYaxis()->SetTitle("# of events >= M_{e#mu}");
+  else emu_ttbar->GetYaxis()->SetTitle("# of events / 10 GeV/c^{2}");
+  emu_ttbar->GetYaxis()->SetTitleSize(0.04);
+  emu_ttbar->GetYaxis()->SetTitleOffset(1.2);
   
   // //
 //   emu_dilepton->SetFillColor(0);
@@ -55,17 +90,20 @@
   emu_ww->SetFillColor(5);
   emu_ww->Draw("HISTsames");
 
-//   emu_tw->SetFillColor(46);
-//   emu_tw->Draw("HISTsames");
+  emu_wz->SetFillColor(8);
+  emu_wz->Draw("HISTsames");
+
+  emu_tw->SetFillColor(46);
+  emu_tw->Draw("HISTsames");
   
   emu_wjet->SetFillColor(4);
   emu_wjet->Draw("HISTsames");
 
-//   emu_zee->SetFillColor(3);
-//   emu_zee->Draw("HISTsames");
+  emu_zmumu->SetFillColor(7);
+  emu_zmumu->Draw("HISTsames");
 
-//   emu_zmumu->SetFillColor(7);
-//   emu_zmumu->Draw("HISTsames");
+  emu_zee->SetFillColor(3);
+  emu_zee->Draw("HISTsames");
 
 //   emu_qcd->SetFillColor(1);
 //   emu_qcd->Draw("HISTsames");
@@ -73,7 +111,13 @@
 
   emu_data->SetLineWidth(2);
   emu_data->SetMarkerStyle(8);
+  emu_data->SetMarkerSize(0.8);
   emu_data->Draw("sames");
+
+  double dataMax = emu_data->GetMaximum();
+  double ttbarMax = emu_ttbar->GetMaximum();
+  if (dataMax > ttbarMax) emu_ttbar->SetMaximum(dataMax * 1.1);
+  //if (emu_data->GetMaximum() > emu_ttbar->GetMaximum()) emu_ttbar->GetYaxis()->SetRangeUser(0., emu_data->GetMaximum() * 1.1);
 
   //Redraw axis
   emu_ttbar->Draw("sameaxis");
@@ -85,8 +129,8 @@
 //   emu_LS->Draw("sames");
   
   //
-  TLegend legend0(0.48,0.60,0.68,0.88);
-  legend0.SetTextSize(0.04);
+  TLegend legend0(0.7, 0.8, 0.88, 0.54);
+  legend0.SetTextSize(0.03);
   legend0.SetFillColor(0);
   
   legend0.AddEntry(emu_data, "Data");
@@ -94,19 +138,14 @@
 //   legend0.AddEntry(emu_ewk, "EWK (MC)");
 //   legend0.AddEntry(emu_jet, "Jet BG (MC)");
 
-  legend0.AddEntry(emu_ttbar, "t #bar{t} (MC)");
-  legend0.AddEntry(emu_ztautau, "Z #rightarrow #tau #tau (MC)");
-  //legend0.AddEntry(emu_ww, "WW, tW (MC)");
-  legend0.AddEntry(emu_ww, "WW (MC)");
-  legend0.AddEntry(emu_wjet, "W+jet + Z #rightarrow #mu #mu (MC)");
-
-  // legend0.AddEntry(emu_ttbar, "t #bar{t} (MC)");
-//   legend0.AddEntry(emu_ztautau, "Z #rightarrow #tau #tau (MC)");
-//   legend0.AddEntry(emu_ww, "WW (MC)");
-//   legend0.AddEntry(emu_tw, "tW (MC)");
-//   legend0.AddEntry(emu_wjet, "W+jets (MC)");
-//   legend0.AddEntry(emu_zee, "Z #rightarrow ee (MC)");
-//   legend0.AddEntry(emu_zmumu, "Z #rightarrow #mu #mu (MC)");
+   legend0.AddEntry(emu_ttbar, "t #bar{t} (MC)");
+   legend0.AddEntry(emu_ztautau, "Z #rightarrow #tau #tau (MC)");
+   legend0.AddEntry(emu_ww, "WW (MC)");
+   legend0.AddEntry(emu_wz, "WZ (MC)");
+   legend0.AddEntry(emu_tw, "tW (MC)");
+   legend0.AddEntry(emu_wjet, "W+jets (MC)");
+   legend0.AddEntry(emu_zmumu, "Z #rightarrow #mu #mu (MC)");
+   legend0.AddEntry(emu_zee, "Z #rightarrow ee (MC)");
 //   legend0.AddEntry(emu_qcd, "Jet BG (MC)");
 
 
@@ -115,19 +154,22 @@
   legend0.SetBorderSize(0);
   legend0.Draw("sames");
   
-  TPaveLabel label0(0.300,0.89,0.740,0.99,"7 TeV, #int L dt = 3190pb^{-1}","brNDC");
-  label0.SetFillColor(0);
-  label0.SetFillStyle(0);
-  label0.SetBorderSize(0);
-  label0.SetTextSize(0.40);
-  label0.Draw("sames");
-
-  TPaveLabel label1(0.300,0.89,0.740,0.99,"CMS Preliminary","brNDC");
+  TPaveLabel label1(0.7, 0.91, 0.9, 0.82, "CMS Preliminary","brNDC");
   label1.SetFillColor(0);
   label1.SetFillStyle(0);
   label1.SetBorderSize(0);
   label1.SetTextSize(0.40);
   label1.Draw("sames");
+
+  stringstream sStream;
+  sStream.str("");
+  sStream << "#sqrt{s} = 7TeV,  #int L dt = " << lumi << "pb^{-1}";
+  TPaveLabel label0(0.3, 0.88, 0.65, 0.78, sStream.str().c_str(), "brNDC");
+  label0.SetFillColor(0);
+  label0.SetFillStyle(0);
+  label0.SetBorderSize(0);
+  label0.SetTextSize(0.40);
+  label0.Draw("sames");
 
 
   
