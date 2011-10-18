0d9e0109eac862f6cd136cc75423c389673e3633
treis <>
2011-10-18 16:46:52 +0000

    - change histogram names

--- HEEPemu/macro_MakeEMuInvMassLSOSPlot.C
+++ HEEPemu/macro_MakeEMuInvMassLSOSPlot.C
@@ -1,14 +1,11 @@
 {
   #include <string>
   #include <sstream>
-
   #include "TH1F.h"
-  //#include "plotstyle.C"
  
   // parameters //////////////////////////////////////////////////////////////
-  // luminosity of data 
-  TFile input("testEmuSpec.root", "open");
-  const float lumi = 3190;
+  TFile input("testEmuSpec3534pb-1.root", "open");
+  const float lumi = 3534;
 
   bool logPlot = false;
   bool integrate = false;
@@ -21,48 +18,50 @@
     // //LOOPING ON BINS
     double error;
     for (int i = 1; i < 101; ++i) {
-      LS_ttbar->SetBinContent(i, LS_ttbar->IntegralAndError(i, 100, error));
-      LS_ttbar->SetBinError(i, error);
-      LS_ztautau->SetBinContent(i, LS_ztautau->IntegralAndError(i, 100, error));
-      LS_ztautau->SetBinError(i, error);
-      LS_ww->SetBinContent(i, LS_ww->IntegralAndError(i, 100, error));
-      LS_ww->SetBinError(i, error);
-      LS_wz->SetBinContent(i, LS_wz->IntegralAndError(i, 100, error));
-      LS_wz->SetBinError(i, error);
-      LS_tw->SetBinContent(i, LS_tw->IntegralAndError(i, 100, error));
-      LS_tw->SetBinError(i, error);
-      LS_wjet->SetBinContent(i, LS_wjet->IntegralAndError(i, 100, error));
-      LS_wjet->SetBinError(i, error);
-      LS_zmum->SetBinContent(i, LS_zmum->IntegralAndError(i, 100, error));
-      LS_zmum->SetBinError(i, error);
-      LS_zele->SetBinContent(i, LS_zele->IntegralAndError(i, 100, error));
-      LS_zele->SetBinError(i, error);
-      LS_data->SetBinContent(i, LS_data->IntegralAndError(i, 100, error));   
-      LS_data->SetBinError(i, error);
+      emuMass_LS_ttbar->SetBinContent(i, emuMass_LS_ttbar->IntegralAndError(i, 100, error));
+      emuMass_LS_ttbar->SetBinError(i, error);
+      emuMass_LS_ztautau->SetBinContent(i, emuMass_LS_ztautau->IntegralAndError(i, 100, error));
+      emuMass_LS_ztautau->SetBinError(i, error);
+      emuMass_LS_ww->SetBinContent(i, emuMass_LS_ww->IntegralAndError(i, 100, error));
+      emuMass_LS_ww->SetBinError(i, error);
+      emuMass_LS_wz->SetBinContent(i, emuMass_LS_wz->IntegralAndError(i, 100, error));
+      emuMass_LS_wz->SetBinError(i, error);
+      emuMass_LS_tw->SetBinContent(i, emuMass_LS_tw->IntegralAndError(i, 100, error));
+      emuMass_LS_tw->SetBinError(i, error);
+      emuMass_LS_wjets->SetBinContent(i, emuMass_LS_wjets->IntegralAndError(i, 100, error));
+      emuMass_LS_wjets->SetBinError(i, error);
+      emuMass_LS_zmumu->SetBinContent(i, emuMass_LS_zmumu->IntegralAndError(i, 100, error));
+      emuMass_LS_zmumu->SetBinError(i, error);
+      emuMass_LS_zee->SetBinContent(i, emuMass_LS_zee->IntegralAndError(i, 100, error));
+      emuMass_LS_zee->SetBinError(i, error);
+      //emuMass_LS_zz->SetBinContent(i, emuMass_LS_zz->IntegralAndError(i, 100, error));
+      //emuMass_LS_zz->SetBinError(i, error);
+      emuMass_LS_data->SetBinContent(i, emuMass_LS_data->IntegralAndError(i, 100, error));   
+      emuMass_LS_data->SetBinError(i, error);
 
-      OS_ttbar->SetBinContent(i, OS_ttbar->IntegralAndError(i, 100, error));
-      OS_ttbar->SetBinError(i, error);
-      OS_ztautau->SetBinContent(i, OS_ztautau->IntegralAndError(i, 100, error));
-      OS_ztautau->SetBinError(i, error);
-      OS_ww->SetBinContent(i, OS_ww->IntegralAndError(i, 100, error));
-      OS_ww->SetBinError(i, error);
-      OS_wz->SetBinContent(i, OS_wz->IntegralAndError(i, 100, error));
-      OS_wz->SetBinError(i, error);
-      OS_tw->SetBinContent(i, OS_tw->IntegralAndError(i, 100, error));
-      OS_tw->SetBinError(i, error);
-      OS_wjet->SetBinContent(i, OS_wjet->IntegralAndError(i, 100, error));
-      OS_wjet->SetBinError(i, error);
-      OS_zmum->SetBinContent(i, OS_zmum->IntegralAndError(i, 100, error));
-      OS_zmum->SetBinError(i, error);
-      OS_zele->SetBinContent(i, OS_zele->IntegralAndError(i, 100, error));
-      OS_zele->SetBinError(i, error);
-      OS_data->SetBinContent(i, OS_data->IntegralAndError(i, 100, error));   
-      OS_data->SetBinError(i, error);
+      emuMass_OS_ttbar->SetBinContent(i, emuMass_OS_ttbar->IntegralAndError(i, 100, error));
+      emuMass_OS_ttbar->SetBinError(i, error);
+      emuMass_OS_ztautau->SetBinContent(i, emuMass_OS_ztautau->IntegralAndError(i, 100, error));
+      emuMass_OS_ztautau->SetBinError(i, error);
+      emuMass_OS_ww->SetBinContent(i, emuMass_OS_ww->IntegralAndError(i, 100, error));
+      emuMass_OS_ww->SetBinError(i, error);
+      emuMass_OS_wz->SetBinContent(i, emuMass_OS_wz->IntegralAndError(i, 100, error));
+      emuMass_OS_wz->SetBinError(i, error);
+      emuMass_OS_tw->SetBinContent(i, emuMass_OS_tw->IntegralAndError(i, 100, error));
+      emuMass_OS_tw->SetBinError(i, error);
+      emuMass_OS_wjets->SetBinContent(i, emuMass_OS_wjets->IntegralAndError(i, 100, error));
+      emuMass_OS_wjets->SetBinError(i, error);
+      emuMass_OS_zmumu->SetBinContent(i, emuMass_OS_zmumu->IntegralAndError(i, 100, error));
+      emuMass_OS_zmumu->SetBinError(i, error);
+      emuMass_OS_zee->SetBinContent(i, emuMass_OS_zee->IntegralAndError(i, 100, error));
+      emuMass_OS_zee->SetBinError(i, error);
+      //emuMass_OS_zz->SetBinContent(i, emuMass_OS_zz->IntegralAndError(i, 100, error));
+      //emuMass_OS_zz->SetBinError(i, error);
+      emuMass_OS_data->SetBinContent(i, emuMass_OS_data->IntegralAndError(i, 100, error));   
+      emuMass_OS_data->SetBinError(i, error);
     }     
   }
   
-  //setPlotStyle();
- 
   TCanvas *cLS = new TCanvas("cLS", "emu Spectrum LS", 100, 100, 800, 600);
   cLS->SetBorderMode(0);
   cLS->SetFrameBorderMode(0);
@@ -86,68 +85,72 @@
   // LS---------
   cLS->cd();
   
-  LS_ttbar->GetXaxis()->SetTitle("M_{e#mu} (GeV/c^{2})");
-  LS_ttbar->GetXaxis()->SetTitleSize(0.04);
-  LS_ttbar->GetXaxis()->SetLabelSize(0.04);
+  emuMass_LS_ttbar->GetXaxis()->SetTitle("M_{e#mu} (GeV/c^{2})");
+  emuMass_LS_ttbar->GetXaxis()->SetTitleSize(0.04);
+  emuMass_LS_ttbar->GetXaxis()->SetLabelSize(0.04);
 
-  if (integrate) LS_ttbar->GetYaxis()->SetTitle("# of LS events >= M_{e#mu}");
-  else LS_ttbar->GetYaxis()->SetTitle("# of LS events / 10 GeV/c^{2}");
-  LS_ttbar->GetYaxis()->SetTitleSize(0.04);
-  LS_ttbar->GetYaxis()->SetTitleOffset(1.2);
-
-  //
-  LS_ttbar->SetFillColor(2);
-  LS_ttbar->Draw("HIST");
-
-  LS_ztautau->SetFillColor(6);
-  LS_ztautau->Draw("HISTsames");
-
-  LS_ww->SetFillColor(5);
-  LS_ww->Draw("HISTsames");
-
-  LS_wz->SetFillColor(8);
-  LS_wz->Draw("HISTsames");
-
-  LS_tw->SetFillColor(46);
-  LS_tw->Draw("HISTsames");
-
-  LS_wjet->SetFillColor(4);
-  LS_wjet->Draw("HISTsames");
-
-  LS_zmum->SetFillColor(7);
-  LS_zmum->Draw("HISTsames");
-
-  LS_zele->SetFillColor(3);
-  LS_zele->Draw("HISTsames");
+  if (integrate) emuMass_LS_ttbar->GetYaxis()->SetTitle("# of LS events >= M_{e#mu}");
+  else emuMass_LS_ttbar->GetYaxis()->SetTitle("# of LS events / 10 GeV/c^{2}");
+  emuMass_LS_ttbar->GetYaxis()->SetTitleSize(0.04);
+  emuMass_LS_ttbar->GetYaxis()->SetTitleOffset(1.2);
 
   //
-  LS_data->SetLineWidth(2);
-  LS_data->SetMarkerStyle(8);
-  LS_data->SetMarkerSize(0.8);
-  LS_data->Draw("sames");
+  emuMass_LS_ttbar->SetFillColor(2);
+  emuMass_LS_ttbar->Draw("HIST");
 
-  double dataMax = LS_data->GetMaximum();
-  double ttbarMax = LS_ttbar->GetMaximum();
-  if (dataMax > ttbarMax) LS_ttbar->SetMaximum(dataMax * 1.1);
-  //if (LS_data->GetMaximum() > LS_ttbar->GetMaximum()) LS_ttbar->GetYaxis()->SetRangeUser(0., LS_data->GetMaximum() * 1.1);
+  emuMass_LS_ztautau->SetFillColor(6);
+  emuMass_LS_ztautau->Draw("HISTsames");
+
+  emuMass_LS_ww->SetFillColor(5);
+  emuMass_LS_ww->Draw("HISTsames");
+
+  emuMass_LS_wz->SetFillColor(8);
+  emuMass_LS_wz->Draw("HISTsames");
+
+  emuMass_LS_tw->SetFillColor(46);
+  emuMass_LS_tw->Draw("HISTsames");
+
+  emuMass_LS_wjets->SetFillColor(4);
+  emuMass_LS_wjets->Draw("HISTsames");
+
+  emuMass_LS_zmumu->SetFillColor(7);
+  emuMass_LS_zmumu->Draw("HISTsames");
+
+  emuMass_LS_zee->SetFillColor(3);
+  emuMass_LS_zee->Draw("HISTsames");
+
+  //emuMass_LS_zz->SetFillColor(9);
+  //emuMass_LS_zz->Draw("HISTsames");
+
+  //
+  emuMass_LS_data->SetLineWidth(2);
+  emuMass_LS_data->SetMarkerStyle(8);
+  emuMass_LS_data->SetMarkerSize(0.8);
+  emuMass_LS_data->Draw("sames");
+
+  double dataMax = emuMass_LS_data->GetMaximum();
+  double ttbarMax = emuMass_LS_ttbar->GetMaximum();
+  if (dataMax > ttbarMax) emuMass_LS_ttbar->SetMaximum(dataMax * 1.1);
+  //if (emuMass_LS_data->GetMaximum() > emuMass_LS_ttbar->GetMaximum()) emuMass_LS_ttbar->SetMaximum(emuMass_LS_data->GetMaximum() * 1.1);
 
   //Redraw axis
-  LS_ttbar->Draw("sameaxis");
+  emuMass_LS_ttbar->Draw("sameaxis");
 
   //
   TLegend legendLS(0.7, 0.8, 0.88, 0.54);
   legendLS.SetTextSize(0.03);
   legendLS.SetFillColor(0);
 
-  legendLS.AddEntry(LS_data, "Data");
-  legendLS.AddEntry(LS_ttbar, "t #bar{t} (MC)");
-  legendLS.AddEntry(LS_ztautau, "Z #rightarrow #tau #tau (MC)");
-  legendLS.AddEntry(LS_ww, "WW, (MC)");
-  legendLS.AddEntry(LS_wz, "WZ, (MC)");
-  legendLS.AddEntry(LS_tw, "tW, (MC)");
-  legendLS.AddEntry(LS_wjet, "W+jet (MC)");
-  legendLS.AddEntry(LS_zmum, "Z #rightarrow #mu #mu (MC)");
-  legendLS.AddEntry(LS_zele, "Z #rightarrow ee (MC)");
+  legendLS.AddEntry(emuMass_LS_data, "Data");
+  legendLS.AddEntry(emuMass_LS_ttbar, "t #bar{t} (MC)");
+  legendLS.AddEntry(emuMass_LS_ztautau, "Z #rightarrow #tau #tau (MC)");
+  legendLS.AddEntry(emuMass_LS_ww, "WW, (MC)");
+  legendLS.AddEntry(emuMass_LS_wz, "WZ, (MC)");
+  legendLS.AddEntry(emuMass_LS_tw, "tW, (MC)");
+  legendLS.AddEntry(emuMass_LS_wjets, "W+jets (MC)");
+  legendLS.AddEntry(emuMass_LS_zmumu, "Z #rightarrow #mu #mu (MC)");
+  legendLS.AddEntry(emuMass_LS_zee, "Z #rightarrow ee (MC)");
+  //legendLS.AddEntry(emuMass_LS_zz, "ZZ, (MC)");
 
   legendLS.SetBorderSize(0);
   legendLS.Draw("sames");
@@ -173,67 +176,71 @@
   // OS-------
   cOS->cd();
 
-  OS_ttbar->GetXaxis()->SetTitle("M_{e #mu} (GeV/c^{2})");
-  OS_ttbar->GetXaxis()->SetTitleSize(0.04);
-  OS_ttbar->GetXaxis()->SetLabelSize(0.04);
+  emuMass_OS_ttbar->GetXaxis()->SetTitle("M_{e #mu} (GeV/c^{2})");
+  emuMass_OS_ttbar->GetXaxis()->SetTitleSize(0.04);
+  emuMass_OS_ttbar->GetXaxis()->SetLabelSize(0.04);
 
-  if (integrate) OS_ttbar->GetYaxis()->SetTitle("# of OS events >= M_{e#mu}");
-  else OS_ttbar->GetYaxis()->SetTitle("# OS events / 10 GeV/c^{2}");
-  OS_ttbar->GetYaxis()->SetTitleSize(0.04);
-  OS_ttbar->GetYaxis()->SetTitleOffset(1.1);
+  if (integrate) emuMass_OS_ttbar->GetYaxis()->SetTitle("# of OS events >= M_{e#mu}");
+  else emuMass_OS_ttbar->GetYaxis()->SetTitle("# OS events / 10 GeV/c^{2}");
+  emuMass_OS_ttbar->GetYaxis()->SetTitleSize(0.04);
+  emuMass_OS_ttbar->GetYaxis()->SetTitleOffset(1.1);
   
-  OS_ttbar->SetFillColor(2);
-  OS_ttbar->Draw("HIST");
+  emuMass_OS_ttbar->SetFillColor(2);
+  emuMass_OS_ttbar->Draw("HIST");
 
-  OS_ztautau->SetFillColor(6);
-  OS_ztautau->Draw("HISTsames");
+  emuMass_OS_ztautau->SetFillColor(6);
+  emuMass_OS_ztautau->Draw("HISTsames");
 
-  OS_ww->SetFillColor(5);
-  OS_ww->Draw("HISTsames");
+  emuMass_OS_ww->SetFillColor(5);
+  emuMass_OS_ww->Draw("HISTsames");
 
-  OS_wz->SetFillColor(8);
-  OS_wz->Draw("HISTsames");
+  emuMass_OS_wz->SetFillColor(8);
+  emuMass_OS_wz->Draw("HISTsames");
 
-  OS_tw->SetFillColor(46);
-  OS_tw->Draw("HISTsames");
+  emuMass_OS_tw->SetFillColor(46);
+  emuMass_OS_tw->Draw("HISTsames");
 
-  OS_wjet->SetFillColor(4);
-  OS_wjet->Draw("HISTsames");
+  emuMass_OS_wjets->SetFillColor(4);
+  emuMass_OS_wjets->Draw("HISTsames");
 
-  OS_zmum->SetFillColor(7);
-  OS_zmum->Draw("HISTsames");
+  emuMass_OS_zmumu->SetFillColor(7);
+  emuMass_OS_zmumu->Draw("HISTsames");
 
-  OS_zele->SetFillColor(3);
-  OS_zele->Draw("HISTsames");
+  emuMass_OS_zee->SetFillColor(3);
+  emuMass_OS_zee->Draw("HISTsames");
+
+  //emuMass_OS_zz->SetFillColor(9);
+  //emuMass_OS_zz->Draw("HISTsames");
 
   //
-  OS_data->SetLineWidth(2);
-  OS_data->SetMarkerStyle(8);
-  OS_data->SetMarkerSize(0.8);
-  OS_data->Draw("sames");
+  emuMass_OS_data->SetLineWidth(2);
+  emuMass_OS_data->SetMarkerStyle(8);
+  emuMass_OS_data->SetMarkerSize(0.8);
+  emuMass_OS_data->Draw("sames");
 
-  dataMax = OS_data->GetMaximum();
-  ttbarMax = OS_ttbar->GetMaximum();
-  if (dataMax > ttbarMax) OS_ttbar->SetMaximum(dataMax * 1.1);
-  //if (OS_data->GetMaximum() > OS_ttbar->GetMaximum()) OS_ttbar->SetMaximum(OS_data->GetMaximum() * 0.1);
+  dataMax = emuMass_OS_data->GetMaximum();
+  ttbarMax = emuMass_OS_ttbar->GetMaximum();
+  if (dataMax > ttbarMax) emuMass_OS_ttbar->SetMaximum(dataMax * 1.1);
+  //if (emuMass_OS_data->GetMaximum() > emuMass_OS_ttbar->GetMaximum()) emuMass_OS_ttbar->SetMaximum(emuMass_OS_data->GetMaximum() * 0.1);
 
   //Redraw axis
-  OS_ttbar->Draw("sameaxis");
+  emuMass_OS_ttbar->Draw("sameaxis");
 
   //
   TLegend legendOS(0.7, 0.8, 0.88, 0.54);
   legendOS.SetTextSize(0.03);
   legendOS.SetFillColor(0);
 
-  legendOS.AddEntry(OS_data, "Data");
-  legendOS.AddEntry(OS_ttbar, "t #bar{t} (MC)");
-  legendOS.AddEntry(OS_ztautau, "Z #rightarrow #tau #tau (MC)");
-  legendOS.AddEntry(OS_ww, "WW, (MC)");
-  legendOS.AddEntry(OS_wz, "WZ, (MC)");
-  legendOS.AddEntry(OS_tw, "tW, (MC)");
-  legendOS.AddEntry(OS_wjet, "W+jet (MC)");
-  legendOS.AddEntry(OS_zmum, "Z #rightarrow #mu #mu (MC)");
-  legendOS.AddEntry(OS_zele, "Z #rightarrow ee (MC)");
+  legendOS.AddEntry(emuMass_OS_data, "Data");
+  legendOS.AddEntry(emuMass_OS_ttbar, "t #bar{t} (MC)");
+  legendOS.AddEntry(emuMass_OS_ztautau, "Z #rightarrow #tau #tau (MC)");
+  legendOS.AddEntry(emuMass_OS_ww, "WW, (MC)");
+  legendOS.AddEntry(emuMass_OS_wz, "WZ, (MC)");
+  legendOS.AddEntry(emuMass_OS_tw, "tW, (MC)");
+  legendOS.AddEntry(emuMass_OS_wjets, "W+jets (MC)");
+  legendOS.AddEntry(emuMass_OS_zmumu, "Z #rightarrow #mu #mu (MC)");
+  legendOS.AddEntry(emuMass_OS_zee, "Z #rightarrow ee (MC)");
+  //legendOS.AddEntry(emuMass_OS_zz, "ZZ, (MC)");
 
   legendOS.SetBorderSize(0);
   legendOS.Draw("sames");
--- HEEPemu/macro_MakeEMuInvMassPlot.C
+++ HEEPemu/macro_MakeEMuInvMassPlot.C
@@ -1,22 +1,18 @@
 {
   #include <string>
   #include <sstream>
-      
   #include "TH1F.h"
-  //#include "plotstyle.C"
       
   // parameters //////////////////////////////////////////////////////////////
-  // luminosity of data 
-  TFile input("testEmuSpec.root", "open");
-  const float lumi = 3190;
+  TFile input("testEmuSpec3534pb-1.root", "open");
+  //TFile input("testEmuSpec_PUcut20_3190pb-1.root","open");
+  
+  const float lumi = 3534;
       
   bool logPlot = false;
   bool integrate = false;
   ////////////////////////////////////////////////////////////////////////////
 
-  TFile input("testEmuSpec.root","open");
-  //TFile input("testEmuSpec_PUcut20_3190pb-1.root","open");
-  
   input.cd();
 
   // INTEGRATING FROM THE RIGHT SIDE!
@@ -24,30 +20,30 @@
     // //LOOPING ON BINS
     double error;
     for (int i = 1; i < 101; ++i) {
-      //emu_ttbar->GetNbinsX();
-      emu_ttbar->SetBinContent(i, emu_ttbar->IntegralAndError(i, 100, error));
-      emu_ttbar->SetBinError(i, error);
-      emu_ztautau->SetBinContent(i, emu_ztautau->IntegralAndError(i, 100, error));
-      emu_ztautau->SetBinError(i, error);
-      emu_ww->SetBinContent(i, emu_ww->IntegralAndError(i, 100, error));
-      emu_ww->SetBinError(i, error);
-      emu_wz->SetBinContent(i, emu_wz->IntegralAndError(i, 100, error));
-      emu_wz->SetBinError(i, error);
-      emu_tw->SetBinContent(i, emu_tw->IntegralAndError(i, 100, error));
-      emu_tw->SetBinError(i, error);
-      emu_wjet->SetBinContent(i, emu_wjet->IntegralAndError(i, 100, error));
-      emu_wjet->SetBinError(i, error);
-      emu_zmumu->SetBinContent(i, emu_zmumu->IntegralAndError(i, 100, error));
-      emu_zmumu->SetBinError(i, error);
-      emu_zee->SetBinContent(i, emu_zee->IntegralAndError(i, 100, error));
-      emu_zee->SetBinError(i, error);
-      emu_data->SetBinContent(i, emu_data->IntegralAndError(i, 100, error));   
-      emu_data->SetBinError(i, error);
+      //emuMass_ttbar->GetNbinsX();
+      emuMass_ttbar->SetBinContent(i, emuMass_ttbar->IntegralAndError(i, 100, error));
+      emuMass_ttbar->SetBinError(i, error);
+      emuMass_ztautau->SetBinContent(i, emuMass_ztautau->IntegralAndError(i, 100, error));
+      emuMass_ztautau->SetBinError(i, error);
+      emuMass_ww->SetBinContent(i, emuMass_ww->IntegralAndError(i, 100, error));
+      emuMass_ww->SetBinError(i, error);
+      emuMass_wz->SetBinContent(i, emuMass_wz->IntegralAndError(i, 100, error));
+      emuMass_wz->SetBinError(i, error);
+      emuMass_tw->SetBinContent(i, emuMass_tw->IntegralAndError(i, 100, error));
+      emuMass_tw->SetBinError(i, error);
+      emuMass_wjets->SetBinContent(i, emuMass_wjets->IntegralAndError(i, 100, error));
+      emuMass_wjets->SetBinError(i, error);
+      emuMass_zmumu->SetBinContent(i, emuMass_zmumu->IntegralAndError(i, 100, error));
+      emuMass_zmumu->SetBinError(i, error);
+      emuMass_zee->SetBinContent(i, emuMass_zee->IntegralAndError(i, 100, error));
+      emuMass_zee->SetBinError(i, error);
+      //emuMass_zz->SetBinContent(i, emuMass_zz->IntegralAndError(i, 100, error));
+      //emuMass_zz->SetBinError(i, error);
+      emuMass_data->SetBinContent(i, emuMass_data->IntegralAndError(i, 100, error));   
+      emuMass_data->SetBinError(i, error);
     }     
   }
  
-  //setPlotStyle();
-
   TCanvas *cEmu = new TCanvas("cEmu", "emu Spectrum", 100, 100, 800, 600);
   cEmu->SetBorderMode(0);
   cEmu->SetFrameBorderMode(0);
@@ -62,14 +58,14 @@
   gStyle->SetTitleXOffset(1.);
   gStyle->SetTitleYOffset(1.3);
   
-  emu_ttbar->GetXaxis()->SetTitle("M_{e#mu} (GeV/c^{2})");
-  emu_ttbar->GetXaxis()->SetTitleSize(0.04);
-  emu_ttbar->GetXaxis()->SetLabelSize(0.04);
+  emuMass_ttbar->GetXaxis()->SetTitle("M_{e#mu} (GeV/c^{2})");
+  emuMass_ttbar->GetXaxis()->SetTitleSize(0.04);
+  emuMass_ttbar->GetXaxis()->SetLabelSize(0.04);
 
-  if (integrate) emu_ttbar->GetYaxis()->SetTitle("# of events >= M_{e#mu}");
-  else emu_ttbar->GetYaxis()->SetTitle("# of events / 10 GeV/c^{2}");
-  emu_ttbar->GetYaxis()->SetTitleSize(0.04);
-  emu_ttbar->GetYaxis()->SetTitleOffset(1.2);
+  if (integrate) emuMass_ttbar->GetYaxis()->SetTitle("# of events >= M_{e#mu}");
+  else emuMass_ttbar->GetYaxis()->SetTitle("# of events / 10 GeV/c^{2}");
+  emuMass_ttbar->GetYaxis()->SetTitleSize(0.04);
+  emuMass_ttbar->GetYaxis()->SetTitleOffset(1.2);
   
   // //
 //   emu_dilepton->SetFillColor(0);
@@ -81,75 +77,65 @@
 //   emu_jet->SetFillColor(3);
 //   emu_jet->Draw("HISTsames");
 
-  emu_ttbar->SetFillColor(2);
-  emu_ttbar->Draw("HIST");
+  emuMass_ttbar->SetFillColor(2);
+  emuMass_ttbar->Draw("HIST");
 
-  emu_ztautau->SetFillColor(6);
-  emu_ztautau->Draw("HISTsames");
+  emuMass_ztautau->SetFillColor(6);
+  emuMass_ztautau->Draw("HISTsames");
 
-  emu_ww->SetFillColor(5);
-  emu_ww->Draw("HISTsames");
+  emuMass_ww->SetFillColor(5);
+  emuMass_ww->Draw("HISTsames");
 
-  emu_wz->SetFillColor(8);
-  emu_wz->Draw("HISTsames");
+  emuMass_wz->SetFillColor(8);
+  emuMass_wz->Draw("HISTsames");
 
-  emu_tw->SetFillColor(46);
-  emu_tw->Draw("HISTsames");
+  emuMass_tw->SetFillColor(46);
+  emuMass_tw->Draw("HISTsames");
   
-  emu_wjet->SetFillColor(4);
-  emu_wjet->Draw("HISTsames");
+  emuMass_wjets->SetFillColor(4);
+  emuMass_wjets->Draw("HISTsames");
 
-  emu_zmumu->SetFillColor(7);
-  emu_zmumu->Draw("HISTsames");
+  emuMass_zmumu->SetFillColor(7);
+  emuMass_zmumu->Draw("HISTsames");
 
-  emu_zee->SetFillColor(3);
-  emu_zee->Draw("HISTsames");
+  emuMass_zee->SetFillColor(3);
+  emuMass_zee->Draw("HISTsames");
 
-//   emu_qcd->SetFillColor(1);
-//   emu_qcd->Draw("HISTsames");
+  //emuMass_zz->SetFillColor(9);
+  //emuMass_zz->Draw("HISTsames");
 
+  emuMass_data->SetLineWidth(2);
+  emuMass_data->SetMarkerStyle(8);
+  emuMass_data->SetMarkerSize(0.8);
+  emuMass_data->Draw("sames");
 
-  emu_data->SetLineWidth(2);
-  emu_data->SetMarkerStyle(8);
-  emu_data->SetMarkerSize(0.8);
-  emu_data->Draw("sames");
-
-  double dataMax = emu_data->GetMaximum();
-  double ttbarMax = emu_ttbar->GetMaximum();
-  if (dataMax > ttbarMax) emu_ttbar->SetMaximum(dataMax * 1.1);
-  //if (emu_data->GetMaximum() > emu_ttbar->GetMaximum()) emu_ttbar->GetYaxis()->SetRangeUser(0., emu_data->GetMaximum() * 1.1);
+  double dataMax = emuMass_data->GetMaximum();
+  double ttbarMax = emuMass_ttbar->GetMaximum();
+  if (dataMax > ttbarMax) emuMass_ttbar->SetMaximum(dataMax * 1.1);
+  //if (emuMass_data->GetMaximum() > emuMass_ttbar->GetMaximum()) emuMass_ttbar->SetMaximum(emuMass_data->GetMaximum() * 1.1);
 
   //Redraw axis
-  emu_ttbar->Draw("sameaxis");
+  emuMass_ttbar->Draw("sameaxis");
 
-//   emu_LS->SetLineWidth(2);
-//   emu_LS->SetLineColor(4);
-//   emu_LS->SetMarkerStyle(23);
-//   emu_LS->SetMarkerColor(4);
-//   emu_LS->Draw("sames");
-  
   //
   TLegend legend0(0.7, 0.8, 0.88, 0.54);
   legend0.SetTextSize(0.03);
   legend0.SetFillColor(0);
   
-  legend0.AddEntry(emu_data, "Data");
+  legend0.AddEntry(emuMass_data, "Data");
 //   legend0.AddEntry(emu_dilepton, "dilepton (MC)");
 //   legend0.AddEntry(emu_ewk, "EWK (MC)");
 //   legend0.AddEntry(emu_jet, "Jet BG (MC)");
 
-   legend0.AddEntry(emu_ttbar, "t #bar{t} (MC)");
-   legend0.AddEntry(emu_ztautau, "Z #rightarrow #tau #tau (MC)");
-   legend0.AddEntry(emu_ww, "WW (MC)");
-   legend0.AddEntry(emu_wz, "WZ (MC)");
-   legend0.AddEntry(emu_tw, "tW (MC)");
-   legend0.AddEntry(emu_wjet, "W+jets (MC)");
-   legend0.AddEntry(emu_zmumu, "Z #rightarrow #mu #mu (MC)");
-   legend0.AddEntry(emu_zee, "Z #rightarrow ee (MC)");
-//   legend0.AddEntry(emu_qcd, "Jet BG (MC)");
-
-
-  //legend0.AddEntry(emu_LS, "2 * Like Sign emu");
+  legend0.AddEntry(emuMass_ttbar, "t #bar{t} (MC)");
+  legend0.AddEntry(emuMass_ztautau, "Z #rightarrow #tau #tau (MC)");
+  legend0.AddEntry(emuMass_ww, "WW (MC)");
+  legend0.AddEntry(emuMass_wz, "WZ (MC)");
+  legend0.AddEntry(emuMass_tw, "tW (MC)");
+  legend0.AddEntry(emuMass_wjets, "W+jets (MC)");
+  legend0.AddEntry(emuMass_zmumu, "Z #rightarrow #mu #mu (MC)");
+  legend0.AddEntry(emuMass_zee, "Z #rightarrow ee (MC)");
+  //legend0.AddEntry(emuMass_zz, "ZZ (MC)");
   
   legend0.SetBorderSize(0);
   legend0.Draw("sames");
@@ -170,9 +156,5 @@
   label0.SetBorderSize(0);
   label0.SetTextSize(0.40);
   label0.Draw("sames");
-
-
-  
-  //gPad->Print("Plot_EMuMethod_R.eps");
   
 }
