12d81b4666654bb092ff1c522f697065b35da207
Thomas Reis <thomas.reis@cern.ch>
2013-10-03 14:25:20 +0200

    split cfg files for different configurations

---
+++ HltGsfTrkAnalyser/test/hltgsftrkanalyser_vsReco_lowEt_cfg.py
@@ -0,0 +1,8 @@
+from hltgsftrkanalyser_lowEt_cfg import *
+
+process.HltGsfTrkAna.referenceTrackCollTag = cms.InputTag('electronGsfTracks', '', 'RECO')
+
+process.HltGsfTrkAna.matchMethod = cms.untracked.uint32(0)      # for matching by dR
+process.HltGsfTrkAna.discrCutForMatch = cms.untracked.double(0.5)
+process.HltGsfTrkAna.largerThan = cms.untracked.bool(False)
+
