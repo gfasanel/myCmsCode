12127c9df7a0be501faad58cb67c6bc67ebf31e7
Thomas Reis <thomas.reis@cern.ch>
2014-05-23 11:48:11 +0200

    save fit functions in root file

--- HEEPemu/macro_mu_ptRes.py
+++ HEEPemu/macro_mu_ptRes.py
@@ -270,7 +270,11 @@ if savePlots:
     for canvas in cList:
         canvas.Write(canvas.GetName())
     c1.Write(c1.GetName())
+    for func in resFitFuncList:
+        func.Write()
     if len(treePrefices) > 1:
         c2.Write(c2.GetName())
+        resErrFitFunc.Write()
+        #resErrFunc.Write()
     output.Close()
 
